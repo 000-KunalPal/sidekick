eb902c21be91add63fca47ad035eac9eb97ceb50
Kunal Pal <kun931pal@gmail.com>
2025-07-08 22:12:58 +0530

    feat(jobs): integrate Inngest for background processing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
   return (
-    <div className='min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16'>
-      hello world
+    <div className='p-4 max-w-7xl mx-auto'>
+      <Button onClick={() => invoke.mutate({ text: "Kunal" })}>
+        Invoke background job
+      </Button>
     </div>
   );
 }
