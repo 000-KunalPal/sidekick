99d1560b79b44d9d907dcb6f722a5063cdf3207a
Kunal Pal <kun931pal@gmail.com>
2025-07-11 21:28:46 +0530

    feat(billing): implemented clerk billing with stripe

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -11,6 +11,8 @@ import { cn } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Form, FormField } from "@/components/ui/form";
+import { Usage } from "./usage";
+import { useRouter } from "next/navigation";
 
 interface MessageFormProps {
   projectId: string;
@@ -25,7 +27,11 @@ const formSchema = z.object({
 
 export const MessageForm = ({ projectId }: MessageFormProps) => {
   const trpc = useTRPC();
+  const router = useRouter();
   const queryClient = useQueryClient();
+
+  const { data: usage } = useQuery(trpc.usage.status.queryOptions());
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -40,9 +46,14 @@ export const MessageForm = ({ projectId }: MessageFormProps) => {
         queryClient.invalidateQueries(
           trpc.messages.getMany.queryOptions({ projectId })
         );
+        queryClient.invalidateQueries(trpc.usage.status.queryOptions());
       },
       onError: (error) => {
         toast.error(error.message);
+
+        if (error.data?.code === "TOO_MANY_REQUESTS") {
+          router.push("/pricing");
+        }
       },
     })
   );
@@ -57,10 +68,16 @@ export const MessageForm = ({ projectId }: MessageFormProps) => {
   const [isFocused, setIsFocused] = useState(false);
   const isPending = createMessage.isPending;
   const isButttonDisabled = isPending || !form.formState.isValid;
-  const showUsage = false;
+  const showUsage = !!usage;
 
   return (
     <Form {...form}>
+      {showUsage && (
+        <Usage
+          points={usage.remainingPoints}
+          msBeforeNext={usage.msBeforeNext}
+        />
+      )}
       <form
         onSubmit={form.handleSubmit(onSubmit)}
         className={cn(
