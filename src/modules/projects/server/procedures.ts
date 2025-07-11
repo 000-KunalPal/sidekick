e63173a5a24cb4798e9f44c84b1f9771de0299b7
Kunal Pal <kun931pal@gmail.com>
2025-07-11 19:41:16 +0530

    feat(auth): implemented and integrated clerk authentication

--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -1,16 +1,16 @@
 import z from "zod";
 import { prisma } from "@/lib/db";
 import { inngest } from "@/inngest/client";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { protectedProcedure, createTRPCRouter } from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
 import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
-  getOne: baseProcedure
+  getOne: protectedProcedure
     .input(z.object({ id: z.string().min(1, "ID is required") }))
-    .query(async ({ input }) => {
+    .query(async ({ input, ctx }) => {
       const existingProject = await prisma.project.findUnique({
-        where: { id: input.id },
+        where: { id: input.id, userId: ctx.auth.userId },
       });
 
       if (!existingProject) {
@@ -22,8 +22,11 @@ export const projectsRouter = createTRPCRouter({
 
       return existingProject;
     }),
-  getMany: baseProcedure.query(async () => {
+  getMany: protectedProcedure.query(async ({ ctx }) => {
     const projects = await prisma.project.findMany({
+      where: {
+        userId: ctx.auth.userId,
+      },
       orderBy: {
         updatedAt: "asc",
       },
@@ -31,7 +34,7 @@ export const projectsRouter = createTRPCRouter({
 
     return projects;
   }),
-  create: baseProcedure
+  create: protectedProcedure
     .input(
       z.object({
         value: z
@@ -40,9 +43,10 @@ export const projectsRouter = createTRPCRouter({
           .max(10000, "Message cannot exceed 10000 characters"),
       })
     )
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
       const createdProject = await prisma.project.create({
         data: {
+          userId: ctx.auth.userId,
           name: generateSlug(2, {
             format: "kebab",
           }),
